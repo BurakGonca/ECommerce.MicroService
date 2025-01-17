15f9dddfa6e70a3987e1df5d4b27c25a99a9e6e2
Burak <goncabrk@gmail.com>
2025-01-17 10:28:26 +0300

    Api controllerlari tamamlandi, catalog konfigürasyonları yapildi.

---
+++ Services/Catalog/ECommerce.Catalog/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using ECommerce.Catalog.Dtos.ProductDtos;
+using ECommerce.Catalog.Services.ProductServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _productService.GetAllProductAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var value = await _productService.GetByIdProductAsync(id);
+            return Ok(value);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return Ok("Ürün başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return Ok("Ürün başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateProductAsync(updateProductDto);
+            return Ok("Ürün başarıyla güncellendi");
+        }
+
+
+    }
+}
--- Services/Catalog/ECommerce.Catalog/Program.cs
+++ Services/Catalog/ECommerce.Catalog/Program.cs
@@ -1,6 +1,35 @@
+using ECommerce.Catalog.Services.CategoryServices;
+using ECommerce.Catalog.Services.ProductDetailServices;
+using ECommerce.Catalog.Services.ProductImageServices;
+using ECommerce.Catalog.Services.ProductServices;
+using ECommerce.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+//DI konfigürasyonlari
+builder.Services.AddScoped<ICategoryService,CategoryService>();    
+builder.Services.AddScoped<IProductService,ProductService>();    
+builder.Services.AddScoped<IProductDetailService,ProductDetailService>();    
+builder.Services.AddScoped<IProductImageService,ProductImageService>();
+
+//Automappper konfigürasyonu
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+//MongoDB database konfigürasyonlarý
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+//burada DatabaseSetting Sinifi içerisindeki proplarin value larini almak için kullanilir
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
