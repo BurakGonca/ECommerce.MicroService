efe45ea0bd2d13b8a016a12bd5b697b142fdc4a8
Burak <goncabrk@gmail.com>
2025-01-22 11:28:34 +0300

    OrderDetail icin Command ve Handler siniflari tamamlandi, Addres ve OrderDetail icin CRUD API'leri yazildi.

---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using ECommerce.Order.Application.Interfaces;
+using ECommerce.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class UpdateOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public UpdateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderDetailCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.OrderDetailId);
+
+            value.OrderingId = command.OrderingId;
+            value.ProductId = command.ProductId;
+            value.ProductPrice = command.ProductPrice;
+            value.ProductName = command.ProductName;
+            value.ProductTotalPrice = command.ProductTotalPrice;
+            value.ProductAmount = command.ProductAmount;
+
+            await _repository.UpdateAsync(value);
+
+        }
+
+    }
+}
