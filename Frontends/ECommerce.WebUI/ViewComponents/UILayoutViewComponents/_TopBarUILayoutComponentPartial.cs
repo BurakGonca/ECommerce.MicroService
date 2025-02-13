c1ded267bc3754a38f7cb7dc67eb7c61d64e1245
Burak <goncabrk@gmail.com>
2025-02-13 20:35:29 +0300

    UI template giydirilmesine baslandi, viewcomponentler olusturuluyor.

---
+++ Frontends/ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_TopBarUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _TopBarUILayoutComponentPartial : ViewComponent
+	{
+
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
