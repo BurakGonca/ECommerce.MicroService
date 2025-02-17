41d7cc166d023d3b6d0dd62c8bff4fd28de6c0a0
Burak <goncabrk@gmail.com>
2025-02-17 17:05:10 +0300

    ProductList, ProductDetail sayfaları getirildi. Sayfalar uygun biçimde components yapısında hazırlandı.

---
+++ Frontends/ECommerce.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+	public class ProductListController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		
+
+	}
+}
