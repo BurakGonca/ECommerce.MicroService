80eb030d58bd04ed4300f89f10cc21d3892ba31b
Burak <goncabrk@gmail.com>
2025-02-21 17:22:32 +0300

    Admin area product CRUD islemleri yapildi.

---
+++ Frontends/ECommerce.DtoLayer/CatalogDtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DtoLayer.CatalogDtos.ProductDtos
+{
+	public class CreateProductDto
+	{
+		public string ProductName { get; set; }
+		public decimal ProductPrice { get; set; }
+		public string ProductImageUrl { get; set; }
+		public string ProductDescription { get; set; }
+
+
+		//relationships
+		public string CategoryId { get; set; }
+	}
+}
