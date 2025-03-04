108313c4ac4f3115cf642819076b5bace8f89649
Burak <goncabrk@gmail.com>
2025-03-04 16:56:44 +0300

    Category tablosuna ImageUrl sütunu eklendi. UI tarafında dynamic olarak gösterildi.

---
+++ Frontends/ECommerce.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.DtoLayer.CatalogDtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ECommerce.WebUI.ViewComponents.DefaultViewComponents
+{
+	public class _CategoriesDefaultComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CategoriesDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7081/api/Categories");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+
+
+	}
+}
