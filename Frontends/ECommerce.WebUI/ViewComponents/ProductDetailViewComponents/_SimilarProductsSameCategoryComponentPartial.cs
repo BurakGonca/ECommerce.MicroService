08d63017c623ba84b6edfb0c1cd742e7e0cdd409
Burak <goncabrk@gmail.com>
2025-03-10 13:04:57 +0300

    Ürüne ait detay sayfası içindeki aynı kategorideki benzer ürünler viewcomponenti tamamlandı, dynamic olarak getirildi.

---
+++ Frontends/ECommerce.WebUI/ViewComponents/ProductDetailViewComponents/_SimilarProductsSameCategoryComponentPartial.cs
@@ -0,0 +1,51 @@
+﻿using ECommerce.DtoLayer.CatalogDtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ECommerce.WebUI.ViewComponents.ProductDetailViewComponents
+{
+	public class _SimilarProductsSameCategoryComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _SimilarProductsSameCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(string id)
+		{
+			var product = await GetProductByIdAsync(id);
+			if (product is null)
+				return View();
+
+			var categoryProducts = await GetProductsByCategoryIdAsync(product.Category.CategoryID);
+			return View(categoryProducts ?? new List<ResultProductWithCategoryDto>());
+		}
+
+		private async Task<ResultProductWithCategoryDto> GetProductByIdAsync(string productId)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync($"https://localhost:7081/api/Product/{productId}");
+
+			if (!response.IsSuccessStatusCode)
+				return null;
+
+			var jsonData = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<ResultProductWithCategoryDto>(jsonData);
+		}
+
+		private async Task<List<ResultProductWithCategoryDto>> GetProductsByCategoryIdAsync(string categoryId)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync($"https://localhost:7081/api/Product/GetProductsWithCategoryByCategoryId?categoryId={categoryId}");
+
+			if (!response.IsSuccessStatusCode)
+				return null;
+
+			var jsonData = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+		}
+
+	}
+}
